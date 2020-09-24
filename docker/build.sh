2e7b4f5cffe0065e10d2eeea8081e2c1ae7d1efa
User Admin gruppo applicativo <dockadmin@arpav-dckwkr2.arpa.local>
2020-09-24 09:32:25 +0200

    Updated build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker build -t aib .
+if [ "$1" = "local" ]; then
+	echo "Building locally --> no proxy setup"
+	docker build -t aib -f ./Dockerfile.local
+else
+	echo "Building with arpa proxy setup"
+	docker build -t aib .
+fi
+
