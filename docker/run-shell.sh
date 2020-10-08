d4ed5e5970a10a7ea70cde8b536e00547f3e4754
User Admin gruppo applicativo <dockadmin@arpav-dckwkr2.arpa.local>
2020-10-08 18:44:51 +0200

    forecast fortran running

--- docker/run-shell.sh
+++ docker/run-shell.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+DOCKER_RUN="docker run -it --rm --name aib-shell-"`date "+%s"`
+
+echo $DOCKER_RUN
+
 if [ "$1" == "v" ]; then
   echo "Work with volume"
-  docker run -it --rm --name aib-shell -v /home/dockadmin/data/fwi:/fwi aib /bin/bash
-else
-  docker run -it --rm --name aib-shell aib /bin/bash
+  $DOCKER_RUN -v /home/dockadmin/data/fwi:/fwi aib /bin/bash
+else 
+  if [ "$1" == "dbg" ]; then
+    $DOCKER_RUN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --security-opt apparmor=unconfined -v /home/dockadmin/data/fwi:/fwi aib /bin/bash
+  else
+    $DOCKER_RUN aib /bin/bash
+  fi
 fi
